86eefd8d736e59db039181b3206a207f9e019aa6
JuliaOldman0 <julia.oldman0@gmail.com>
2024-03-13 10:13:06 -0400

    added Github Url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://gleeful-cuchufli-b3c8bf.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Julia Oldman
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/JuliaOldman0/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
