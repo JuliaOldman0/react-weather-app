144b6343bbc2f40accbb423d81f474765a0e2514
JuliaOldman0 <julia.oldman0@gmail.com>
2024-03-13 10:31:17 -0400

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://gleeful-cuchufli-b3c8bf.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Julia Oldman
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/JuliaOldman0/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://gleeful-cuchufli-b3c8bf.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Julia Oldman
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/JuliaOldman0/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
