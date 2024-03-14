ee632958dac6818051ab5d5f441f6813b556f978
JuliaOldman0 <julia.oldman0@gmail.com>
2024-03-14 18:58:21 -0400

    added useEffect for live forecast on each day when you search

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() =>{
+setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -17,14 +21,13 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-
-if (index < 5) {
-  return (
-    <div className="rol" key={index}>
-      <WeatherForecastDay data={dailyForecast} />
-    </div>
-  );
-}
+            if (index < 5) {
+              return (
+                <div className="rol" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
           })}
         </div>
       </div>
